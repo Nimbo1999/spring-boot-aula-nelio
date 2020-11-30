0784fa58c2b1cc1a87277c0ac1c2d823405889de
Matheus Lopes <matlopes1999@gmail.com>
2020-11-29 22:58:30 -0300

    Remove imports não utilizados

--- src/main/java/dev/devlopes/shopping/ShoppingApplication.java
+++ src/main/java/dev/devlopes/shopping/ShoppingApplication.java
@@ -1,13 +1,11 @@
 package dev.devlopes.shopping;
 
 import java.util.Arrays;
-import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.crossstore.HashMapChangeSet;
 
 import dev.devlopes.shopping.domain.Categoria;
 import dev.devlopes.shopping.domain.Cidade;
